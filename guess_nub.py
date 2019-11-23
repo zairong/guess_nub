165e0b9416bf58f1d89b86f495e467153b28e614
zairong <justiceman3211@hotmail.com>
2019-11-23 20:04:49 +0800

    guess1

---
+++ guess_nub.py
@@ -0,0 +1,13 @@
+import random
+r = random.randint(1,100)
+while True:
+	num = input('請猜數字:')
+	num = int(num)
+	if num == r:
+		print('你猜中了!')
+		break
+	elif num < r:
+		print('你的數字>',num)
+	else:
+		print('你的數字<',num)
+
