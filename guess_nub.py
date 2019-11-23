bd3d0df3d38dc4a28094366dcccc1f2e6e9845a2
zairong <justiceman3211@hotmail.com>
2019-11-23 20:38:31 +0800

    final version

--- guess_nub.py
+++ guess_nub.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100)
+start = input('請決定隨機數字開始值:')
+end = input('請決定隨機數字結束值:')
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
 count = 0 
 while True:
 	count += 1 #count = count + 1
@@ -9,9 +13,11 @@ while True:
 		print('你猜中了!')
 		print('這是你猜的第', count, '次')
 		break
-	elif num < r:
+	elif num < r and num > start - 1 :
 		print('正確答案>',num)
-	else:
+	elif num > r and num < end + 1 :
 		print('正確答案<',num)
+	else:
+		print('數值錯誤,請輸入', start , '至', end , '的數值')
 	print('這是你猜的第', count, '次')	
 
