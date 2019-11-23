83bb84780544eb4d8c1885d3504e319f93f8eaa3
zairong <justiceman3211@hotmail.com>
2019-11-23 20:14:08 +0800

    add count part

--- guess_nub.py
+++ guess_nub.py
@@ -1,13 +1,16 @@
 import random
 r = random.randint(1,100)
+count = 0 
 while True:
+	count += 1 #count = count + 1
 	num = input('請猜數字:')
 	num = int(num)
 	if num == r:
 		print('你猜中了!')
 		break
 	elif num < r:
-		print('你的數字>',num)
+		print('正確答案>',num)
 	else:
-		print('你的數字<',num)
+		print('正確答案<',num)
+	print('這是你猜的第', count, '次')	
 
